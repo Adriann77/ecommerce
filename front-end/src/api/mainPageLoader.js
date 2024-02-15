8ebdd8964ea003acfb51ce1023aa77305926c20e
Adrian <klimasa98@gmail.com>
2024-02-15 18:58:51 +0100

    connect router w/ localhost

---
+++ front-end/src/api/mainPageLoader.js
@@ -0,0 +1,14 @@
+import { redirect } from 'react-router-dom';
+import { PATH_TO_ENDPOINT_MAPPING } from '../constants/api';
+import { BACK_END_URL } from '../constants/api';
+
+export function mainPageLoader({ params }) {
+
+	const backendPath = PATH_TO_ENDPOINT_MAPPING[params.gender];
+
+	if (backendPath) {
+		return fetch(`${BACK_END_URL}/${backendPath}`);
+	} else {
+		return redirect('/kobieta');
+	}
+}
